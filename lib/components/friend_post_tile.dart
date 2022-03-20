de78ad0d935e895469ac6ba9f832acc37e52d3da
ramz101 <86409116+ramz101@users.noreply.github.com>
2022-03-20 14:35:39 -0700

    heavy

--- lib/components/friend_post_tile.dart
+++ lib/components/friend_post_tile.dart
@@ -1,3 +1,40 @@
 import 'package:flutter/material.dart';
 
-import '/components/components.dart';
+import 'components.dart';
+import '/models/models.dart';
+
+class FriendPostListView extends StatelessWidget {
+  
+  final List<Post> friendPost;
+
+  const FriendPostListView({ 
+    Key? key,
+    required this.friendPost,
+   }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 16,
+        right: 16,
+        top: 0
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Social Chefs',
+            style: Theme.of(context).textTheme.headline1,
+          ),
+          const SizedBox(height: 16,),
+          //Add the postlistview 
+
+          const SizedBox(height: 16,)
+        ],
+      ),
+    );
+  }
+}
+
+
